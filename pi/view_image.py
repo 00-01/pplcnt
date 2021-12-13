760d3b283dc286e48186c8f24831b2a26ee558be
00-01 <59557934+00-01@users.noreply.github.com>
2021-12-13 23:01:51 +0900

    Update view_image.py

--- pi/view_image.py
+++ pi/view_image.py
@@ -1,35 +1,13 @@
 import numpy as np
 import cv2
-from glob import glob
 
 
-# filename = "IR_MVPC10_0004_20211206-14_07_54.bin"
-filename = "IR_MVPC10_0004_20211206-14_13_04.bin"
-calibration = "Calibration.bin"
+filename = "IR_MVPC10_0004_20211213-13_43_46.bin"
 
-raw = []
-with open(filename, mode='rb') as f:
-    # raw = np.fromfile(f, dtype=np.int16)
-    byte = f.read(2)
-    while byte:
-        byte_to_int = int.from_bytes(byte, byteorder="little", signed=True)
-        raw.append(byte_to_int)
-        byte = f.read(2)
-raw_to_shape = np.reshape(raw, (80, 80))
+raw = np.fromfile(filename, dtype=np.uint16).astype(np.uint8)
 
-# cal = []
-with open(calibration, mode='rb') as f:
-    cal = np.fromfile(f, dtype=np.uint16)
-    # byte = c.read(2)
-    # while byte:
-    #     byte_to_int = int.from_bytes(byte, byteorder="little", signed=False)
-    #     cal.append(byte_to_int)
-    #     byte = c.read(2)
-cal_to_shape = np.reshape(cal, (80, 80))
+raw_to_shape = np.reshape(raw[:6400], (80, 80))
 
-calibratedImage = np.subtract(raw, cal)
-calibratedImage_to_shape = np.reshape(calibratedImage, (80, 80))
+cv2.imwrite("0.png", raw_to_shape)
 
-cv2.imwrite(f"{filename}.jpg", calibratedImage_to_shape)
-cv2.imshow("asdf", calibratedImage_to_shape)
 cv2.waitKey()
